d6a15299be52a1b01c722db4ee5a1ebef1b51b19
Sapan Bhatia <gwsapan@gmail.com>
2017-02-07 16:57:42 -0800

    Logstash_tail updated

--- patterns.py
+++ patterns.py
@@ -1,19 +1,28 @@
 from pyparsing import *
 
 class Parser:
-    ansible = "TASK [" + SkipTo(']',include=True) + SkipTo(LineEnd()) + oneOf('changed ok failed error unreachable') + ': ' + SkipTo(LineEnd())
-    ansible.setParseAction(lambda x:{"task":x[1],"status":x[4],"subject": x[6],"ansible":1})
-    
+    test_pattern = "{{{" + SkipTo("}}}",include=True)
+    test_pattern.setParseAction(lambda x:{"test":x[1][0]})
+
+    test_pattern2 = "1" + SkipTo(LineEnd(),include=True) + SkipTo(LineEnd(),include=True) + "2"
+    test_pattern2.setParseAction(lambda x:{"test":x[1]})
+
+    ansible = "TASK [" + SkipTo(']',include=True) + SkipTo(LineEnd(),include=True) + oneOf('changed ok failed error unreachable') + ': ' + SkipTo(LineEnd(),include=True)
+    ansible.setParseAction(lambda x:{"task":x[1],"status":x[3],"subject": x[5],"ansible":1})
+
+    ansible2 = "TASK [" + SkipTo(']',include=True) + SkipTo(LineEnd(),include=True) + SkipTo(LineEnd(),include=True) + oneOf('changed ok failed error unreachable') + ': ' + SkipTo(LineEnd(),include=True)
+    ansible2.setParseAction(lambda x:{"task":x[1],"status":x[2],"subject": x[4],"ansible":1})
+
     # Vagrant
-    vagrant_start = Literal("[") + SkipTo("]",include=True) + "Importing base box '" + SkipTo("'",include=True) + SkipTo(LineEnd())
+    vagrant_start = Literal("[") + SkipTo("]",include=True) + "Importing base box '" + SkipTo("'",include=True) + SkipTo(LineEnd(),include=True)
     vagrant_start.setParseAction(lambda x:{"vm":x[1][0],"image":x[3][0],"vagrant":1,"global":"Booting VM"})
 
-    vagrant_start = Literal("[") + SkipTo("]",include=True) + "Machine booted and ready!" + SkipTo(LineEnd())
+    vagrant_start = Literal("[") + SkipTo("]",include=True) + "Machine booted and ready!" + SkipTo(LineEnd(), include=True)
     vagrant_start.setParseAction(lambda x:{"vm":x[1][0], "vagrant":1, "global":"VM Booted up"})
 
-    gradle_start = "Downloading https://services.gradle.org" + SkipTo(LineEnd())
+    gradle_start = "Downloading https://services.gradle.org" + SkipTo(LineEnd(), include=True)
     gradle_start.setParseAction(lambda x:{"gradle":1, "global":"Gradle started"})
 
-    debian_preparing = "Preparing to unpack" + Word(printables) + SkipTo(LineEnd())
+    debian_preparing = "Preparing to unpack" + Word(printables) + SkipTo(LineEnd(), include=True)
     debian_preparing.setParseAction(lambda x:{"debian":1, "package":x[1]})
 
